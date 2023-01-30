260c8b2f41273b44efa073056dffc344b9b21827
Matheus Markies <matheus.markies@gmail.com>
2023-01-30 10:02:58 -0300

    Start

--- src/main/java/com/matheusmarkies/Main.java
+++ src/main/java/com/matheusmarkies/Main.java
@@ -1,5 +1,7 @@
-package com.matheusmarkies;public class Main {
+package com.matheusmarkies;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
--- src/main/java/com/matheusmarkies/input/InputMathExpression.java
+++ src/main/java/com/matheusmarkies/input/InputMathExpression.java
@@ -1,2 +1,16 @@
-package com.matheusmarkies.input;public class InputMathExpression {
+package com.matheusmarkies.input;
+
+public class InputMathExpression {
+
+    String vars[] = {
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+            "y", "z",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+            "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
+            "Y", "Z"
+    };
+
+
+
 }
