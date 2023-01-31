38d461e988d3c6e6a8a13629c29450adbbd2ba6d
Matheus Markies <matheus.markies@gmail.com>
2023-01-31 13:44:25 -0300

    Start

--- src/main/java/com/matheusmarkies/Main.java
+++ src/main/java/com/matheusmarkies/Main.java
@@ -6,4 +6,4 @@ public class Main {
     public static void main(String[] args) {
         getEq("y = 2 + x + (o+(k+1))+5[6k+1]+{3gh+5+(5s+0)}(6+8)1/8 + (6+8)h + (8 + (4 + h + (g+h)))");
     }
-}
+}//
